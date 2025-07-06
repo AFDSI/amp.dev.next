18066d6fc3b9063d685da66d41c7e50552935e77
jay gray <jaygray0919@gmail.com>
2025-07-06 18:06:50 -0500

    Feat: restored packArtifacts

--- gulpfile.js/build.js
+++ gulpfile.js/build.js
@@ -295,31 +295,31 @@ function buildPrepare(done) {
       importAll,
       zipTemplates
     ),
+// ... (inside buildPrepare function)
     // eslint-disable-next-line prefer-arrow-callback
     async function packArtifacts() {
       // Store everything built so far for later stages to pick up
       // Local path to the archive containing artifacts of the first stage
-      // Comment out or remove these unused variable declarations:
-      // const SETUP_ARCHIVE = 'artifacts/setup.tar.gz';
-      /*
-       * Comment out the entire SETUP_STORED_PATHS array block
-       * to avoid SyntaxError: Unexpected token ']'
-       */
-      // const SETUP_STORED_PATHS = [
-      //   './pages/content/',
-      //   './pages/shared/',
-      //   './dist/',
-      //   './boilerplate/lib/',
-      //   './boilerplate/dist/',
-      //   './playground/dist/',
-      //   './frontend21/dist/',
-      //   './.cache/',
-      //   './examples/static/samples/samples.json',
-      // ];
-      // await sh(`mkdir -p artifacts`); // Commented out this line
-      // await sh(`tar cfj ${SETUP_ARCHIVE} ${SETUP_STORED_PATHS.join(' ')}`); // Commented out this line
+      const SETUP_ARCHIVE = 'artifacts/setup.tar.gz'; // Uncomment this line
+      // All paths that contain altered files at build setup time
+      // eslint-disable-next-line no-unused-vars
+      const SETUP_STORED_PATHS = [ // Uncomment this line and the array content
+        './pages/content/',
+        './pages/shared/',
+        './dist/',
+        './boilerplate/lib/',
+        './boilerplate/dist/',
+        './playground/dist/',
+        './frontend21/dist/',
+        './.cache/',
+        './examples/static/samples/samples.json',
+      ];
+
+      await sh(`mkdir -p artifacts`); // Uncomment this line
+      await sh(`tar cfj ${SETUP_ARCHIVE} ${SETUP_STORED_PATHS.join(' ')}`); // Uncomment this line
     }, // This comma is important if packArtifacts is part of a series/parallel array
-    // eslint-disable-next-line prefer-arrow-callback
+// ... (rest of the buildPrepare function)
+
     function exit(_done) {
       done();
       _done();
@@ -327,7 +327,6 @@ function buildPrepare(done) {
     }
   )(done);
 }
-
 /**
  * Fetches remote artifacts that have been built in earlier stages
  *
